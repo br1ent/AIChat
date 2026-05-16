17a82d08ee2b8d94bd35d40ff55abf0d731f2031
石铭轩 <530220399@qq.com>
2026-05-16 17:36:08 +0800

    修复聊天框输入的问题，当用户输入空内容的时候，再输入信息无法发送的问题

--- frontend/src/components/character/chat_field/input_field/InputField.vue
+++ frontend/src/components/character/chat_field/input_field/InputField.vue
@@ -15,11 +15,12 @@ function focus() {
 }
 
 async function sendMessage() {
+  const content = message.value.trim();
+  if (!content) return;
+
   if (isProcessing) return;
   isProcessing = true;
 
-  const content = message.value.trim();
-  if (!content) return;
   message.value = "";
 
   emit("pushBackMessage", {role: 'user', content: content, id: crypto.randomUUID()}) // 添加用户信息
