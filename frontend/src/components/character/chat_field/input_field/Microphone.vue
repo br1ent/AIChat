7fd057bfcf13e78ab3603f3c82204e0fb4d37e62
石铭轩 <530220399@qq.com>
2026-05-17 15:51:15 +0800

    实现语音合成后端

--- frontend/src/components/character/chat_field/input_field/Microphone.vue
+++ frontend/src/components/character/chat_field/input_field/Microphone.vue
@@ -56,12 +56,10 @@ const sendToBackend = async (arrayBuffer) => {
   try {
     const res = await api.post('/api/friend/message/asr/asr/', formData);
     const data = res.data;
-    console.log(data);
     if (data.result === "success") {
       emit('send', null, data.text)
     }
   } catch (err) {
-    console.error(err);
   }
 };
 
