aef0ba9a766f12957a2c0909c3a702c890dc2794
石铭轩 <530220399@qq.com>
2026-05-18 16:47:24 +0800

    全部功能实现完成,已上线

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=$)7fmrvnpez%v+=#o4_@o*#du*ir(dsa!g!-hlc3l)#jfh91n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'app4397.acapp.acwing.com.cn']
 
